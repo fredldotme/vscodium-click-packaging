7a1dba27cdfe05eae127a08914839420d5e1b547
Alfred Neumayer <dev.beidl@gmail.com>
2022-03-07 13:44:36 +0100

    treewide: Fix multiarch and scaling

--- build.sh
+++ build.sh
@@ -4,23 +4,28 @@ sed -e
 
 rm *.tar.gz
 
-pkgname=vscodium
-pkgver=1.65.0
-srcdir=$ROOT/build/aarch64-linux-gnu/app
-pkgdir=$srcdir/install
-mkdir -p $srcdir $pkgdir
-
-wget https://github.com/VSCodium/vscodium/releases/download/$pkgver/VSCodium-linux-$1-$pkgver.tar.gz
-mkdir -p $srcdir/vscodium/VSCodium-linux-$1-$pkgver
-tar xvf ./VSCodium-linux-$1-$pkgver.tar.gz -C $pkgdir
+dlarch="$1"
+if [ "$1" == "amd64" ]; then
+    dlarch="x64"
+fi
 
 CLICK_ARCH=$(dpkg-architecture -qDEB_HOST_ARCH)
 CLICK_FRAMEWORK=ubuntu-sdk-16.04.5
+
+pkgver=1.65.0
+srcdir=$BUILD_DIR
+pkgdir=$INSTALL_DIR
+mkdir -p $srcdir $pkgdir
+
+wget https://github.com/VSCodium/vscodium/releases/download/$pkgver/VSCodium-linux-$dlarch-$pkgver.tar.gz
+tar xvf ./VSCodium-linux-$dlarch-$pkgver.tar.gz -C $pkgdir
 
 cp $ROOT/manifest.json $pkgdir/
 sed -i "s/@CLICK_ARCH@/$CLICK_ARCH/g" $pkgdir/manifest.json
 sed -i "s/@CLICK_FRAMEWORK@/$CLICK_FRAMEWORK/g" $pkgdir/manifest.json
 cp $ROOT/codium.apparmor $pkgdir/
 cp $ROOT/codium.desktop $pkgdir/
+cp $ROOT/codium.wrapper $pkgdir/
+chmod a+x $pkgdir/codium.wrapper
 
 exit 0
